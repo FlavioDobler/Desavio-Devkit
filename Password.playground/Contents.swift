e94c8ad5975baec695a66c84db680c86075f0f78
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2023-03-28 14:41:02 -0300

    Finalizando

--- Password.playground/Contents.swift
+++ Password.playground/Contents.swift
@@ -5,9 +5,11 @@ let lowerCase = CharacterSet.lowercaseLetters
 let upperCase = CharacterSet.uppercaseLetters
 let numbers = CharacterSet.decimalDigits
 let spaces = CharacterSet.whitespaces
-let letters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+let special = CharacterSet(charactersIn: "!@#$%^&*()_+-=[]\\{}|;':\",./<>?")
 
 func valPassword ( _ password: String!) -> Bool {
+    //No minimo 3 characteres
+    guard password.count >= 4 else { return false }
     
     //Primeira Letra Maiuscula!
     if password.prefix(1) != (password.prefix(1)).uppercased() {
@@ -15,28 +17,37 @@ func valPassword ( _ password: String!) -> Bool {
         return false
     }
     
-    //Verifica se tem letras minusculas, numeros, espaços em branco e cumprimento!
+    //Verifica especiais
+    if password.rangeOfCharacter(from: special) != nil {
+        print("Não atende os requisitos!!")
+        return false
+    }
+    
+    //Verifica se tem letras minusculas, numeros e espaços em branco.
     if !password.unicodeScalars.contains(where:{ lowerCase.contains($0)}) &&
         !password.unicodeScalars.contains(where:{ numbers.contains($0)}) &&
         !(password.unicodeScalars.contains(where: {spaces.contains($0)})) &&
-        password.count <= 15 && password.count >= 5 &&
-        password.rangeOfCharacter(from: letters.inverted) == nil {
+        password.count <= 15 && password.count >= 5 {
         print("Não atende os requisitos")
         return false
     }
     
-  //  Verifica sequencias
-        password.forEach { char in
-            if char.isWholeNumber {
-                if char.wholeNumberValue == String(Int(password[char])) + 1{
-                    print("Não atende os requisitos")
-                }
-            }
+    //Verfica sequencias
+    for i in 0...(password.count - 4) {
+        let currentChar = password[password.index(password.startIndex, offsetBy: i+3)]
+        let nextChar = password[password.index(password.startIndex, offsetBy: i+2)]
+        let nextNextChar = password[password.index(password.startIndex, offsetBy: i+1)]
+        guard let currentNum = Int(String(currentChar)),
+              let nextNum = Int(String(nextChar)),
+              let nextNextNum = Int(String(nextNextChar)) else { continue }
+        if currentNum - nextNum == 1 && nextNum - nextNextNum == 1 {
+            print("Não atende os requisitos")
+            return false
         }
-        print("Atende os requisitos!!")
-        return true
     }
-
-let pass1 = "Tentativa235"
+    print("Atende os requisitos!!")
+    return true
+}
+let pass1 = "Tenta123tiva23"
 valPassword(pass1)
 
